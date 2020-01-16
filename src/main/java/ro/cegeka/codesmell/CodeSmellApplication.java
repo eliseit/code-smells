bcb7c7697751c720910ab0ab7a84aa8335988c45
Elisei Tataru <elisei.tataru@vdab.be>
2020-01-16 17:09:24 +0200

    Initial commit

---
+++ src/main/java/ro/cegeka/codesmell/CodeSmellApplication.java
@@ -0,0 +1,13 @@
+package ro.cegeka.codesmell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSmellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeSmellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/cegeka/codesmell/CodeSmellApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.cegeka.codesmell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSmellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
